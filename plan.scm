271b2acc70b23af7150e615a733e82dd673db84d
Greshbolt <greshbolt@gmail.com>
2019-11-21 22:36:08 -0500

    goddammit all

--- plan.scm
+++ plan.scm
@@ -1,7 +1,7 @@
 #!/usr/bin/env scheme-r5rs
 
 ;Print output from interpreting a given list of PLAN programs
-;Usage: ./plan.scm [filename]
+;Usage: ./plan.scm [filename] 
 (define main
   (lambda (args)
     (load "myfns.ss") ;File defining myinterpreter
